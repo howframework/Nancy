4c25bcff46ee9812c8257611f8d8ca3b6e8ac432
Ikhwan Hayat <ikhwanhayat@gmail.com>
2012-05-16 13:09:12 +0800

    Yellow screen of death

---
+++ src/NancyBlog.Web/YsodErrorHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.ErrorHandling;
+using Nancy;
+
+namespace NancyBlog
+{
+    /// <summary>
+    /// Cuz we like our errors to blow up in royal yellowish goodness.
+    /// </summary>
+    public class YsodErrorHandler : IErrorHandler
+    {
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.InternalServerError;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            object errorObject;
+            context.Items.TryGetValue(NancyEngine.ERROR_EXCEPTION, out errorObject);
+            var exception = errorObject as Exception;
+
+            if (exception != null)
+            {
+                throw exception;
+            }
+        }
+    }
+}
